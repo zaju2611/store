7b4d47215060de6065bad1543a5ef45cdd393ba5
Jakub Zając <jakub.zajac2611@gmail.com>
2023-07-24 19:25:50 +0200

    added promo code adding to shopping cart

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function OrderSummary({
 	value,
@@ -6,9 +6,13 @@ export default function OrderSummary({
 	selectedPayment,
 }) {
 	const [promoCode, setPromoCode] = useState("");
+	const [appliedPromoMessage, setAppliedPromoMessage] = useState("");
+	const [invalidPromoMessage, setInvalidPromoMessage] = useState("");
+	const [isApplied, setIsApplied] = useState(false);
 
 	let deliveryCost =
 		selectedDelivery === "inpost" ? 15 : selectedDelivery === "dpd" ? 17 : 0;
+
 	const paymentFee =
 		selectedPayment === "Przelewy24"
 			? 0
@@ -19,12 +23,51 @@ export default function OrderSummary({
 	if (value > 2000) {
 		deliveryCost = 0;
 	}
-	const promoValueMultiplier = promoCode === "promo" ? 0.9 : 1;
-	const discountedValue = value * promoValueMultiplier;
 
 	const totalDeliveryCost = deliveryCost + paymentFee;
+	const [totalAmount, setTotalAmount] = useState(value + totalDeliveryCost);
 
-	const totalAmount = discountedValue + totalDeliveryCost;
+	const handlePromoCodeChange = (event) => {
+		setPromoCode(event.target.value);
+	};
+
+	const checkPromoCode = () => {
+		if (promoCode === "promo") {
+			const promoValueMultiplier = 0.9;
+			const discountedValue = value * promoValueMultiplier;
+			const newTotalAmount = discountedValue + totalDeliveryCost;
+			setTotalAmount(Math.floor(newTotalAmount));
+			setAppliedPromoMessage("Code applied");
+			setInvalidPromoMessage("");
+			setIsApplied(true);
+			setTimeout(() => {
+				setAppliedPromoMessage("");
+			}, 3000);
+		} else {
+			setInvalidPromoMessage("Invalid promo code");
+			setAppliedPromoMessage("");
+			setTimeout(() => {
+				setInvalidPromoMessage("");
+				setPromoCode("");
+			}, 3000);
+		}
+	};
+
+	const clearPromoCode = () => {
+		setPromoCode("");
+		setTotalAmount(value + totalDeliveryCost);
+		setAppliedPromoMessage("");
+		setIsApplied(false);
+	};
+
+	useEffect(() => {
+		const totalAmountWithPromo =
+			promoCode === "promo"
+				? Math.floor(value * 0.9 + totalDeliveryCost)
+				: value + totalDeliveryCost;
+
+		setTotalAmount(totalAmountWithPromo);
+	}, [value, totalDeliveryCost]);
 
 	return (
 		<div style={{ margin: "20px", width: "50%" }}>
@@ -48,9 +91,29 @@ export default function OrderSummary({
 			</div>
 			<div className="row">
 				<label className="promo-label">
-					<input type="text" placeholder="Promo code:" className="promo" />
+					<input
+						type="text"
+						placeholder="Promo code:"
+						className="promo"
+						value={promoCode}
+						onChange={handlePromoCodeChange}
+					/>
 				</label>
-
+				{isApplied && (
+					<button
+						style={{
+							background: "transparent",
+							border: "2px solid var(--grey)",
+							padding: "5px",
+							width: "10%",
+							cursor: "pointer",
+							margin: "0 10px",
+						}}
+						className="promoButton"
+						onClick={clearPromoCode}>
+						&#x2716;
+					</button>
+				)}
 				<button
 					style={{
 						background: "transparent",
@@ -59,8 +122,53 @@ export default function OrderSummary({
 						width: "20%",
 						cursor: "pointer",
 					}}
-					className="promoButton">
+					className="promoButton"
+					onClick={checkPromoCode}>
 					Add
+				</button>
+			</div>
+			{appliedPromoMessage && (
+				<p
+					style={{
+						color: "green",
+						fontSize: "0.7rem",
+						textAlign: "center",
+						width: "70%",
+						margin: "0 0 1rem 0",
+					}}>
+					{appliedPromoMessage}
+				</p>
+			)}
+			{invalidPromoMessage && (
+				<p
+					style={{
+						color: "red",
+						fontSize: "0.7rem",
+						textAlign: "center",
+						width: "70%",
+						margin: "0 0 1.05rem 0",
+					}}>
+					{invalidPromoMessage}
+				</p>
+			)}
+			<div
+				style={{
+					width: "100%",
+					display: "flex",
+					justifyContent: "flex-end",
+					marginTop:
+						appliedPromoMessage || invalidPromoMessage ? "1rem" : "1.9rem",
+				}}>
+				<button
+					style={{
+						background: "var(--pink",
+						border: "2px solid var(--grey)",
+						padding: "5px",
+						width: "50%",
+						cursor: "pointer",
+					}}
+					className="promoButton">
+					Proceed to checkout
 				</button>
 			</div>
 		</div>
