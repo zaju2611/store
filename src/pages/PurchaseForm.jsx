fbfab2427c62d98ce9465f3b0b90d72091b1b0ee
Jakub Zając <jakub.zajac2611@gmail.com>
2023-10-02 18:32:52 +0200

    validate form on purchase page

--- src/pages/PurchaseForm.jsx
+++ src/pages/PurchaseForm.jsx
@@ -7,24 +7,95 @@ import {
 	buttonClicked,
 	resetButtonClicked,
 } from "../store/reducers/validationSlice";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 import { setFormData, setAddressData } from "../store/reducers/dataSlice";
+import { useNameValidation } from "../hooks/useNameValidation";
+import { useAddressValidation } from "../hooks/useAddressValidation";
+import { useZipCodeValidation } from "../hooks/useZipCodeValidation";
+import { usePhoneValidation } from "../hooks/usePhoneValidation";
+import { useEmailValidation } from "../hooks/useEmailValidation";
+import { useCharacterValidation } from "../hooks/useCharacterValidation";
+import { useNavigate } from "react-router-dom";
 
 export default function PurchaseForm() {
+	const navigate = useNavigate();
+	const { checkName } = useNameValidation();
+	const { checkNumberWithApartment } = useAddressValidation();
+	const { checkZipCode } = useZipCodeValidation();
+
 	const dispatch = useDispatch();
 
 	const formData = useSelector((state) => state.data.formData);
 	const addressData = useSelector((state) => state.data.addressData);
 
-	const [validationContactErrors, setValidationContactErrors] = useState(4);
-	const [validationAddressErrors, setValidationAddressErrors] = useState(4);
+	const [validationErrors, setValidationErrors] = useState({
+		contact: 4,
+		address: 4,
+	});
+
+	const validateFormAddress = (e) => {
+		let err = 0;
+
+		if (!checkName(addressData.street) || addressData.street === "") {
+			err++;
+		}
+		if (!checkName(addressData.city) || addressData.city === "") {
+			err++;
+		}
+		if (
+			!checkNumberWithApartment(addressData.houseNumber) ||
+			addressData.houseNumber === ""
+		) {
+			err++;
+		}
+		if (!checkZipCode(addressData.zipCode) || addressData.zipCode === "") {
+			err++;
+		}
+
+		setValidationErrors({ ...validationErrors, address: err });
+	};
+
+	const { checkCharacters } = useCharacterValidation();
+	const { checkEmail } = useEmailValidation();
+
+	const { checkPhoneNumber } = usePhoneValidation();
+
+	const validateFormRegister = (e) => {
+		let err = 0;
+
+		if (!checkCharacters(formData.name) || formData.name === "") {
+			err++;
+		}
+		if (!checkCharacters(formData.surname) || formData.surname === "") {
+			err++;
+		}
+		if (!checkEmail(formData.email) || formData.email === "") {
+			err++;
+		}
+		if (
+			!checkPhoneNumber(formData.phoneNumber) ||
+			formData.phoneNumber === ""
+		) {
+			err++;
+		}
+
+		setValidationErrors({ ...validationErrors, contact: err });
+	};
+
+	useEffect(() => {
+		validateFormRegister();
+	}, [formData]);
+
+	useEffect(() => {
+		validateFormAddress();
+	}, [addressData]);
 
 	const handleSubmitClick = () => {
 		dispatch(buttonClicked());
 
-		if (validationContactErrors === 0 && validationAddressErrors === 0) {
-			console.log("no errors");
+		if (validationErrors.contact === 0 && validationErrors.address === 0) {
+			navigate("/orderConfirmation");
 		}
 
 		setTimeout(() => {
@@ -38,12 +109,12 @@ export default function PurchaseForm() {
 			<ConcactForm
 				formData={formData}
 				setFormData={(data) => dispatch(setFormData(data))}
-				onErrorCount={setValidationContactErrors}
+				onErrorCount={setValidationErrors}
 			/>
 			<AddressForm
 				addressData={addressData}
 				setAddressData={(data) => dispatch(setAddressData(data))}
-				onErrorCount={setValidationAddressErrors}
+				onErrorCount={setValidationErrors}
 			/>
 			<div
 				style={{
@@ -53,14 +124,12 @@ export default function PurchaseForm() {
 					alignItems: "center",
 					width: "80%",
 				}}>
-				<Link to="/orderConfirmation">
-					<button
-						className="accountBtns"
-						style={{ width: "20%", backgroundColor: "var(--pink)" }}
-						onClick={handleSubmitClick}>
-						Next
-					</button>
-				</Link>
+				<button
+					className="accountBtns"
+					style={{ width: "20%", backgroundColor: "var(--pink)" }}
+					onClick={handleSubmitClick}>
+					Next
+				</button>
 
 				<div>
 					<FaStarOfLife style={{ fontSize: ".5rem", color: "red" }} />
